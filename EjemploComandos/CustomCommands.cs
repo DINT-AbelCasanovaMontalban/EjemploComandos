1347f4edd5104ded6c5d2d437c7e2ac5d5910501
AbelCasanovaMontalban <abel.casanova@iesdoctorbalmis.com>
2019-12-03 12:26:58 +0100

    Agregar archivos de proyecto.

---
+++ EjemploComandos/CustomCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploComandos
+{
+    public static class CustomCommands
+    {
+
+        public static readonly System.Windows.Input.RoutedUICommand Exit = new System.Windows.Input.RoutedUICommand(
+            "Exit",
+            "Exit",
+            typeof(CustomCommands),
+            new System.Windows.Input.InputGestureCollection()
+            {
+                new System.Windows.Input.KeyGesture(System.Windows.Input.Key.S,System.Windows.Input.ModifierKeys.Control)
+            }
+            );
+    }
+}
---
+++ EjemploComandos/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EjemploComandos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            TextBlock textBlock = new TextBlock();
+            textBlock.Text = "Item añadido a las "+ DateTime.Now.Hour + ":" + DateTime.Now.Minute + ":" + DateTime.Now.Second;
+            ItemsListBox.Items.Add(textBlock);
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void CommandBinding_Executed_1(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CommandBinding_CanExecute_1(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+
+
+}
